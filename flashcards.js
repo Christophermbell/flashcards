f324631413af5ad302b3764cf918e4bae9d9dd43
Christopher <christophermarkbell@gmail.com>
2017-07-01 09:08:30 -0700

    first commit

---
+++ flashcards.js
@@ -0,0 +1,11 @@
+function FlashCard(front, back) {
+
+  if (!(this instanceof FlashCard)) {
+    return new FlashCard(front, back);
+  }
+
+  this.front = front;
+  this.back = back;
+}
+
+module.exports = FlashCard;
